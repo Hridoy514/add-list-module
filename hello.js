0d4c065a94e5d8e867b6eb1984fe2f1c3236626b
Md-Hridoy <mdhridoybd514@gmail.com>
2023-03-10 07:36:51 +0600

    project complete

---
+++ hello.js
@@ -0,0 +1,48 @@
+let addBtn = document.getElementById("add-btn");
+let parentList = document.getElementById("parentList")
+addBtn.addEventListener("click", e=>{
+
+    if(parentList.children[0].className == "emptyMsg"){
+        parentList.children[0].remove();
+    }
+        let currentBtn = e.currentTarget;
+    let currentInput = currentBtn.previousElementSibling;
+
+    let newLi = document.createElement("li");
+    newLi.className = "list-group-item d-flex justify-content-between";
+    newLi.innerHTML = `<h3 class=" flex-grow-1">${currentInput.value}</h3>
+                <button class="btn btn-warning mx-3" onclick = "editChapter(this)">Edit</button>
+                <button class="btn btn-danger" onclick = "removeChapter(this)" >Remove</button>`;
+    parentList.appendChild(newLi);
+    document.getElementById("type-text").value = "";
+
+})
+function removeChapter(currElement){
+    currElement.parentElement.remove();
+    let parentList = document.getElementById("parentList")
+    if(parentList.children.length <= 0){
+        let newEmtyMsg = document.createElement("h3");
+        newEmtyMsg.classList.add("emptyMsg")
+        newEmtyMsg.textContent = "Nothing Is Here Please Add Module....";
+        parentList.appendChild(newEmtyMsg);
+    }
+}
+function editChapter(currElement){
+    if(currElement.textContent =="Done"){
+        currElement.textContent ="Edit";
+        let currChapterName = currElement.previousElementSibling.value;
+        let currHeading = document.createElement("h3");
+        currHeading.className = ("flex-grow-1");
+        currHeading.textContent = currChapterName;
+        currElement.parentElement.replaceChild(currHeading,currElement.previousElementSibling);
+    }else{
+        currElement.textContent ="Done";
+        let currChapterName = currElement.previousElementSibling.textContent;
+        let currInput = document.createElement("input");
+        currInput.type = ("text");
+        currInput.placeholder = ("Module Name");
+        currInput.className = ("form-control");
+        currInput.value = currChapterName;
+        currElement.parentElement.replaceChild(currInput,currElement.previousElementSibling);
+    }
+}
